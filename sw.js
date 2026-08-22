e6bcf4c176abbee4359bbc34e0ba89927503af38
hargen1946 <ichiromtfuji@gmail.com>
2026-08-22 17:57:36 +0900

    Use jsQR with stop-after-scan workflow

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='medicine-notebook-v2';
-const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','icon.svg','https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js'];
+const CACHE='medicine-notebook-v4';
+const ASSETS=['./','index.html','styles.css?v=4','app.js?v=4','jsQR.js?v=1.4.0','manifest.webmanifest','icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./'))))});
